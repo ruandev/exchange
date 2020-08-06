aed507692c3b23420c71d6204d8821737e1a1616
Ruan Victor <ruan.techno@gmail.com>
2020-08-06 20:21:14 -0300

    Add exchange table

---
+++ src/main/kotlin/dev/ruanvictor/database/ExchangeTable.kt
@@ -0,0 +1,24 @@
+package dev.ruanvictor.database
+
+import org.jetbrains.exposed.dao.id.IntIdTable
+import org.jetbrains.exposed.sql.`java-time`.datetime
+import java.math.BigDecimal
+import java.time.LocalDateTime
+
+object ExchangeTable : IntIdTable("exchange") {
+    val userId = integer("user_id")
+    val createdAt = datetime("created_at")
+    val originCurrency = varchar("origin_currency", 3)
+    val originValue = decimal("origin_value", 15, 2)
+    val targetCurrency = varchar("target_currency", 3)
+    val rate = decimal("rate", 15, 2)
+}
+
+data class ExchangeRecord(
+    val user: Int,
+    val originCurrency: String,
+    val originValue: BigDecimal,
+    val targetCurrency: String,
+    val rate: BigDecimal,
+    val createdAt: LocalDateTime
+)
